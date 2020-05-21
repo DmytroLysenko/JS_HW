57354aab0dfe8930fe36b9dd024f37d2d22d441e
DmytroLysenko <DmytroLysenko@users.noreply.github.com>
2020-05-21 21:51:46 +0300

    task 1 of HW-11

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './css/styles.css';
-import createMenuTo from './utils/createMenuTo';
-import initTheme from './utils/initTheme';
-
-const menu = document.querySelector('ul.js-menu');
-createMenuTo(menu);
-initTheme();
